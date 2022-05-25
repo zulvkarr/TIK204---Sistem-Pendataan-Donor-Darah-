92122abd41e6c393a2c20adbcfa5534a3cb958c8
Restu Alsyafiq Selian <restu.m20@mhs.unsyiah.ac.id>
2022-05-25 16:03:34 +0700

    menambahkan fitur profil dan menu logout

---
+++ projek yang sudah fix/welcome.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TAHAP PEMBUATAN</title>
+</head>
+<body>
+    <?php echo "<h1>Welcome " . $_SESSION['username'] . "</h1>"; ?>
+    <H1>HALAMAN INI BELUM KAMI BUAT</H1>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
